35fc87ebea918965225f035916fb4d59e4c69ca8
adityanegara <aditnegarabisnis@gmail.com>
2021-06-05 20:10:44 +0700

    Fix modal bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,41 @@
-
-
 const bookTitleInput = document.getElementById("bookTitle");
 const bookAuthor = document.getElementById("bookAuthor");
 const releaseYear = document.getElementById("releaseYear");
 const alreadyRead = document.getElementById("alreadyRead");
 const createForm = document.getElementById("createForm");
-
-
-document.addEventListener("DOMContentLoaded", function(){
-
-  
-  createForm.addEventListener("submit", function(event){
-    event.preventDefault();
-    
-  });
-});
-
-const modalEditButton = document.querySelector(".modal-edit-button");
+const modalEditButtons = document.querySelectorAll(".modal-edit-button");
 const modalEditBackground = document.querySelector(".modal-edit-background");
 const modalEditClose = document.querySelector(".modal-edit-close");
-modalEditButton.addEventListener("click", function(){
-  modalEditBackground.classList.add('modal-edit-background-active');
-})
-modalEditClose.addEventListener("click", function(){
-  modalEditBackground.classList.remove('modal-edit-background-active');
-})
-const modalDeleteButton = document.querySelector(".modal-delete-button");
+const modalDeleteButtons = document.querySelectorAll(".modal-delete-button");
 const modalDeleteBackground = document.querySelector(".modal-delete-background");
 const modalDeleteClose = document.querySelector(".modal-delete-close");
 
-modalDeleteButton.addEventListener("click", function(){
-  modalDeleteBackground.classList.add('modal-delete-background-active');  
+document.addEventListener("DOMContentLoaded", function(){
+  createForm.addEventListener("submit", function(event){
+    event.preventDefault();
+  });
+});
+
+
+modalEditButtons.forEach(modalEditButton => {
+  modalEditButton.addEventListener("click", function(){
+    modalEditBackground.classList.add('modal-edit-background-active');
+  })
+});
+
+modalDeleteButtons.forEach(modalDeleteButton=>{
+  modalDeleteButton.addEventListener("click", function(){
+    modalDeleteBackground.classList.add('modal-delete-background-active');  
+  })
+  
 })
+
+
+
+modalEditClose.addEventListener("click", function(){
+  modalEditBackground.classList.remove('modal-edit-background-active');
+})
+
 
 modalDeleteClose.addEventListener("click", function(){
   modalDeleteBackground.classList.remove('modal-delete-background-active');
